3e4e08b6df3840bf017b3cc008dee53199b1251b
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-07-18 21:15:49 +0530

    Added swap pair problem in file

---
+++ 13-Linked-List/swap_pairs.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution{
+public:
+    ListNode* swapPairs(ListNode* head){
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* first = head;
+        ListNode* second = head->next;
+        ListNode* prev = NULL;
+
+        while(first != NULL && second != NULL){
+            ListNode* third = second->next;
+            second->next = first;
+            first->next = third;
+
+            if(prev != NULL){
+                prev->next = second;
+            }
+            else{
+                head = second;
+            }
+
+            prev = first;
+            first = third;
+            
+            if(third != NULL){
+                second = third->next;
+            }
+            else{
+                second = NULL;
+            }
+        }
+    return head;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    Solution obj;
+    ListNode* res = obj.swapPairs(head);
+
+    while(res != NULL){
+        cout << res->val << " ";
+        res = res->next;
+    }
+    cout << "\n";
+
+    return 0;
+}
