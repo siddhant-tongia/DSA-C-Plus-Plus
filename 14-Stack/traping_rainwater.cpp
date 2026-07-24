324862c5a5cf9372e1d162927f1e044319ae06ca
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-07-24 19:46:48 +0530

    Added traping rainwater problem using stack in it

--- 14-Stack/traping_rainwater.cpp
+++ 14-Stack/traping_rainwater.cpp
@@ -1,67 +1,37 @@
-/*#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
-int trap(vector<int> &height){
-    int n = height.size();
-    int ans = 0;
-
-    vector<int> lmax;
-    vector<int> rmax;
-
-    for(int i=1 ; i<n ; i++){
-        lmax[i] = max(height[i],lmax[i-1]);
-    }
-    for(int i=n-2 ; i>=0 ; i--){
-        rmax[i] = max(height[i],rmax[i+1]);
-    }
-    for(int i=0 ; i<n ; i++){
-        ans += min(lmax[i],rmax[n-i-1]) - height[i];
-    }
-return ans;
-}
-int main()
-{
-    vector<int> boundaries = {4,2,0,3,2,5};
-    int result = trap(boundaries);
-
-    cout<<result;
-return 0;
-} */
-
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <algorithm>
-
 using namespace std;
 
 int trap(vector<int>& height){
     int ans = 0;
-    int n = height.size();
+    int current = 0;
+    stack<int> s;
+    
+    while (current < height.size()){
+        while (!s.empty() && height[current] > height[s.top()]){
+            int top = s.top();
+            s.pop();
+            
+            if (s.empty()){
+                break;
+            }
 
-    int l = 0 , r = n-1;
-
-    int lmax = 0;
-    int rmax = 0;
-
-    while(l < r){
-        lmax = max(lmax,height[l]);
-        rmax = max(rmax,height[r]);
-
-        if(lmax < rmax){
-            ans += lmax - height[l];
-            l++;
-        }else{
-            ans += rmax - height[r];
-            r--;
+            int distance = current - s.top() - 1;      
+            int bounded_height = min(height[current], height[s.top()]) - height[top];
+            
+            ans += distance*bounded_height;
         }
+        s.push(current);
+        current++;
     }
 return ans;
 }
 
 int main(){
-    vector<int> height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-    int total_water = trap(height);
-    cout << "Total water trapped: " << total_water << endl;
+    vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1}; 
+    int total_water = trap(height); 
+    cout << total_water;
 return 0;
 }
