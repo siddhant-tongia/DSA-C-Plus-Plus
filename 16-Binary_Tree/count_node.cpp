a8ff09753bb785bf6f3fbe2d0fcd507dfcacac5b
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-08-02 17:58:02 +0530

    added count node  problem

---
+++ 16-Binary_Tree/count_node.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val){
+            data = val;
+            left = right = NULL;
+        }
+};
+
+static int idx = -1;
+Node* buildtree(vector<int> &preorder){
+    idx++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildtree(preorder);
+    root->right = buildtree(preorder);
+    return root;
+}
+
+int node(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftcount = node(root->left);
+    int rightcount = node(root->right);
+
+    return leftcount + rightcount + 1;
+}
+ 
+int main()
+{
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root = buildtree(preorder);
+    cout << node(root) << endl;
+return 0;
+}
