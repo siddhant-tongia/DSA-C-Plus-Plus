6395a91a0f4e9193f1bb47dd947fe6ec10d3e97e
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-08-04 19:35:52 +0530

    added top view problem in folder

--- 16-Binary_Tree/top_view.cpp
+++ 16-Binary_Tree/top_view.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<map>
 #include<algorithm>
 using namespace std;
 
@@ -27,20 +28,41 @@ Node* buildtree(vector<int> &preorder){
     root->right = buildtree(preorder);
     return root;
 }
-int sum(Node* root){
+void topview(Node* root){
     if(root == NULL){
-        return 0;
+        return;
     }
-    int leftsum = sum(root->left);
-    int rightsum = sum(root->right);
+    queue<pair<Node*,int>> q;
+    map<int,int> m;
+
+    q.push({root,0});
     
-return leftsum + rightsum + root->data;
+    while(q.size() > 0){
+        Node* curr = q.front().first;
+        int currHD = q.front().second;
+        q.pop();
+
+        if(m.find(currHD) == m.end()){
+            m[currHD] = curr->data;
+        }
+
+        if(curr->left != NULL){
+            q.push({curr->left,currHD-1});
+        }
+        if(curr->right != NULL){
+            q.push({curr->right,currHD+1});
+        }
+    }
+
+    for(auto val : m){
+        cout << val.second << " ";
+    }
 }
 
 int main()
 {
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root = buildtree(preorder);
-    cout << sum(root) << endl;
+    topview(root);
 return 0;
 }
