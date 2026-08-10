caf2fb6e55e4de04794e3f3dc6b562e6574abce0
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-08-10 07:47:04 +0530

    added sorted array to balanced BST problem

---
+++ 17-Binary-Search-Tree/sorted_array_to_balanced_BST.cpp
@@ -0,0 +1,49 @@
+    #include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val){
+            data = val;
+            right = left = NULL;
+        }
+};
+    
+Node* helper(vector<int>& nums, int st, int end){
+    int mid = st+(end-st)/2;
+
+    if(st > end){
+        return NULL;
+    }
+
+    Node* root = new Node(nums[mid]);
+
+    root->left = helper(nums, st, mid-1);
+    root->right = helper(nums, mid+1, end);
+
+return root;
+}
+
+Node* sortedArrayToBST(vector<int>& nums){
+    Node* root = helper(nums,0,nums.size()-1);
+return root;
+}
+
+void inorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main(){
+    vector<int> nums = {-10,3,0,5,9};
+    inorder(sortedArrayToBST(nums));
+}
