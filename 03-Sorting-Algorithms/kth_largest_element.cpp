38a91904dfaaa20d66532273b087f90eaa8ec216
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-11 08:44:49 +0530

    docs(kth_largest_element): add problem description and example block

--- 03-Sorting-Algorithms/kth_largest_element.cpp
+++ 03-Sorting-Algorithms/kth_largest_element.cpp
@@ -1,3 +1,11 @@
+/*
+ * Problem: Kth Largest Element in an Array
+ * Description: Given an integer array nums and an integer k, return the kth largest element in the array.
+ * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ * Example:
+ * Input: nums = [3, 2, 1, 5, 6, 4], k = 2
+ * Output: 5
+ */
 #include <iostream>
 #include <vector>
 #include <queue>
