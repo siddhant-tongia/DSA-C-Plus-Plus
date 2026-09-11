fb051d31d0702b5e5ad7fd107afc4d7380048180
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-11 08:14:21 +0530

    docs(insertion_sort): add problem description and example block

--- 03-Sorting-Algorithms/insertion_sort.cpp
+++ 03-Sorting-Algorithms/insertion_sort.cpp
@@ -1,3 +1,11 @@
+/*
+ * Problem: Insertion Sort
+ * Description: Implement Insertion Sort to sort an array of integers in ascending order.
+ * Insertion sort works by building the sorted array one element at a time, picking the next element and inserting it into its correct position in the already sorted part.
+ * Example:
+ * Input: vec = [4, 2, 5, 3, 1]
+ * Output: [1, 2, 3, 4, 5]
+ */
 #include<iostream>
 #include<vector>
 using namespace std;
