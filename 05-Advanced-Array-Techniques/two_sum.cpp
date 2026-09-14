50e610b952b9e4125e2dc35e49d48d5ee16afee4
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-14 09:01:16 +0530

    Refactor: Add problem description block for Two Sum

--- 05-Advanced-Array-Techniques/two_sum.cpp
+++ 05-Advanced-Array-Techniques/two_sum.cpp
@@ -1,3 +1,10 @@
+/*
+ * Problem: Two Sum
+ * Description: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * Example:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0, 1]
+ */
 #include<iostream>
 #include<vector>
 #include<unordered_map>
