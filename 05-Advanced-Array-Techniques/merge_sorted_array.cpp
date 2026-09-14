4c19e8d9853e88781b647a168fa9d1e73ec3fafd
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-14 09:32:03 +0530

    Refactor: Add problem description block for Merge Sorted Array

--- 05-Advanced-Array-Techniques/merge_sorted_array.cpp
+++ 05-Advanced-Array-Techniques/merge_sorted_array.cpp
@@ -1,3 +1,10 @@
+/*
+ * Problem: Merge Sorted Array
+ * Description: You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n. Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * Example:
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ */
 #include<iostream>
 #include<vector>
 using namespace std;
