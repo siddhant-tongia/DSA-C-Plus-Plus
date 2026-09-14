f61b686334020ee6687db75aae4f062d508b23c2
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-14 08:27:17 +0530

    Fix: correctly initialize max_sum in Kadane's algorithm to handle all-negative arrays

--- 05-Advanced-Array-Techniques/kadanes_algorithm.cpp
+++ 05-Advanced-Array-Techniques/kadanes_algorithm.cpp
@@ -1,10 +1,19 @@
+/*
+ * Problem: Maximum Subarray (Kadane's Algorithm)
+ * Description: Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ * Example:
+ * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * Output: 6
+ */
 #include<iostream>
+#include<climits>
 using namespace std;
  
 int main()
 {
     int a[]={1,2,3,4,5};
-    int max_sum,sum=0;
+    int max_sum = INT_MIN;
+    int sum=0;
     for(int i=0;i<5;i++)
     {
         sum+=a[i];
@@ -14,5 +23,7 @@ int main()
             sum=0;
         }
     }
+
+    cout << max_sum;
 return 0;
 }
