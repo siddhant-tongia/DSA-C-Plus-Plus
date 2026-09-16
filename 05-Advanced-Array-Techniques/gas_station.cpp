6bf6f248c50b93eaea125772bb7ac1bde6062987
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-16 20:39:43 +0530

    refactor(gas_station): revise and re-code greedy approach for gas station problem

--- 05-Advanced-Array-Techniques/gas_station.cpp
+++ 05-Advanced-Array-Techniques/gas_station.cpp
@@ -1,3 +1,10 @@
+/*
+ * Problem: Gas Station
+ * Description: There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i]. You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. Return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1.
+ * Example:
+ * Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+ * Output: 3
+ */
 #include <iostream>
 #include <vector>
 using namespace std;
