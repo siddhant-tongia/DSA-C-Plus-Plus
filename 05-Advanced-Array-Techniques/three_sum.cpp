2b6c59a4ef3ff8b53147f4155c512abdcb456d80
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-19 08:08:17 +0530

    docs: add problem description block to 3Sum solution

--- 05-Advanced-Array-Techniques/three_sum.cpp
+++ 05-Advanced-Array-Techniques/three_sum.cpp
@@ -1,4 +1,11 @@
 /*
+ * Problem: 3Sum
+ * Description: Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * Example:
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ */
+/*
 #include<iostream>
 #include<algorithm>
 #include<vector>
