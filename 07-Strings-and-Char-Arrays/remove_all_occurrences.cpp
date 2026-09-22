a99d38a3a183877f07b6a3f0859ceeb2e2dfacae
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-22 07:54:10 +0530

    docs(remove_all_occurrences): Add problem description header with example for Remove All Occurrences of a Substring

--- 07-Strings-and-Char-Arrays/remove_all_occurrences.cpp
+++ 07-Strings-and-Char-Arrays/remove_all_occurrences.cpp
@@ -1,3 +1,10 @@
+/*
+ * Problem: Remove All Occurrences of a Substring
+ * Description: Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed: Find the leftmost occurrence of the substring part and remove it from s. Return s after removing all occurrences of part.
+ * Example:
+ * Input: s = "daabcbaabcbc", part = "abc"
+ * Output: "dab"
+ */
 #include<iostream>
 using namespace std;
 string removeoccurrences(string str,string substr){
