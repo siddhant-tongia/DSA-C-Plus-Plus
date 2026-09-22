63a9a1d07523813b66540606465c493dc15e4fae
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-22 07:47:24 +0530

    docs(reverse_string): Add problem description header with example for Reverse String

--- 07-Strings-and-Char-Arrays/reverse_string.cpp
+++ 07-Strings-and-Char-Arrays/reverse_string.cpp
@@ -1,3 +1,10 @@
+/*
+ * Problem: Reverse String
+ * Description: Write a function that reverses a string. The input string is given as an array of characters s.
+ * Example:
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ */
 #include<iostream>
 using namespace std;
 string Reverse(string str){
