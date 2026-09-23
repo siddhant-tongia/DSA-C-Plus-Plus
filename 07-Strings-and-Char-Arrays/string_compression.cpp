ae08fd5812e1c094878bd92fb47059ba6c7838db
siddhant-tongia <tongiasiddhant42@gmail.com>
2026-09-23 07:54:03 +0530

    Add problem description header to String Compression (Run-Length Encoding) solution - Added structured comment block with problem statement, input/output examples, and algorithm description for the two-pointer in-place string compression approach

--- 07-Strings-and-Char-Arrays/string_compression.cpp
+++ 07-Strings-and-Char-Arrays/string_compression.cpp
@@ -1,3 +1,10 @@
+/*
+ * Problem: String Compression
+ * Description: Given an array of characters chars, compress it using the following algorithm: Begin with an empty string s. For each group of consecutive repeating characters in chars: If the group's length is 1, append the character to s. Otherwise, append the character followed by the group's length. Return the new length of the array.
+ * Example:
+ * Input: chars = ["a","a","b","b","c","c","c"]
+ * Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+ */
 #include<iostream>
 #include<vector>
 using namespace std;
